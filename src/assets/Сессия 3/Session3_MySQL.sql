f8e363c15e412e2f37d6ac9a97aa89a5facb2147
megas781 <megas781@gmail.com>
2022-09-30 14:19:57 +0300

    some edits (хз)

--- src/assets/Сессия 3/Session3_MySQL.sql
+++ src/assets/Сессия 3/Session3_MySQL.sql
@@ -11,6 +11,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+create schema `session3_xx`;
+
 use `session3_xx`;
 
 DROP TABLE IF EXISTS `aircrafts`;
